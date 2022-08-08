91cd1edd42ee507066a482bf6c9b46c39ccbba45
Mateus Marques <95579801+mateusmarquesm@users.noreply.github.com>
2022-08-07 22:24:19 -0300

    Objetivos

---
+++ GC-bot.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+'''
+1 - A acessar o link de transmissão no YouTube Studio
+2 - Adquirir os dados do evento/live (Nome, Horário de ínicio, chave de transmissão e canal)
+3 - Passar esses dados para um template de mensagem
+'''
